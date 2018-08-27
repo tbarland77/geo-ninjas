e07e8b29ff753c12be5dc31c3e4061c0ea49f490
Timothy Barland <tbarland77@hotmail.com>
2018-08-27 19:11:07 -0400

    added map stub from the google maps api

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="map">
-        <h2>Map</h2>
+        <div class="google-map" id="map"></div>
     </div>
 </template>
 
@@ -9,13 +9,38 @@ export default {
     name: 'GMap',
     data() {
         return {
-
+            lat: 53,
+            lng: -60
         }
+    },
+    methods: {
+        renderMap() {
+            const map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: this.lat , lng: this.lng },
+                zoom: 6,
+                maxZoom: 15,
+                minZoom: 3,
+                streetViewControl: false
+            })
+        }
+    },
+    mounted() {
+        this.renderMap()
     }
 }
 </script>
 
 <style>
+.google-map {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    background: #ffff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+}
 
 </style>
 
