8b3dc23dcf33a3f00b8e7f45c9c80557bb7e2297
Timothy Barland <tbarland77@hotmail.com>
2018-08-28 18:56:13 -0400

    added signup to the router and started working on the signup page

---
+++ src/components/auth/Signup.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="signup container">
+        <form @submit.prevent="signup" class="card-panal">
+            <h2 class="center deep-purple-text">Signup dog!</h2>
+            <div class="field">
+                <label for="email">Email:</label>
+                <input type="email" name="email" v-model="email">
+            </div>
+              <div class="field">
+                <label for="password">Password:</label>
+                <input type="password" name="password" v-model="password">
+            </div>
+             <div class="field">
+                <label for="alias">Alias:</label>
+                <input type="text" name="alias" v-model="alias">
+            </div>
+            <div class="field center">
+                <button class="btn deep-purple">Signup dog!</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Signup',
+    data() {
+        return {
+            email: null,
+            password: null,
+            alias: null
+        }
+    },
+    methods: {
+        signup() {
+            //firebase auth here
+        }
+    }
+}
+</script>
+
+<style>
+    .signup {
+        max-width: 400px;
+        margin-top: 60px;
+    }
+    .signup h2 {
+        font-size: 2.4em;
+    }
+    .signup .field {
+        margin-bottom: 16px;
+    }
+</style>
+
+
